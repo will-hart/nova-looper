b99ad5f42a535b83e5d59124a561233cd8268e17
William Hart <391950+will-hart@users.noreply.github.com>
2025-08-03 22:56:29 +1000

    Decrease the size of the tail
    Still trying to work out if its worth it :D
    Maybe the tail could be the power bar?
    
    Signed-off-by: William Hart <391950+will-hart@users.noreply.github.com>

--- src/player.rs
+++ src/player.rs
@@ -28,7 +28,7 @@ pub(super) fn plugin(app: &mut App) {
     app.register_type::<PlayerPower>();
     app.register_type::<ShieldAlarm>();
 
-    app.add_plugins((assets::plugin, trail::TrailPlugin::<25>));
+    app.add_plugins((assets::plugin, trail::TrailPlugin::<12>));
 
     app.add_systems(OnEnter(Screen::Gameplay), spawn_player);
     app.add_systems(
